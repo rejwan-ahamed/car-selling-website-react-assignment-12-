8f22c3a28e8ad4732aa2be855033d2962bcbea2b
rejwan-ahamed <rejwanahamed85@gmail.com>
2022-11-24 08:05:19 +0600

    modal added

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -170,95 +170,123 @@ const Details = () => {
                     leaveTo="opacity-0 scale-95"
                   >
                     <Dialog.Panel className="w-full max-w-md transform overflow-hidden rounded-2xl bg-white p-6 text-left align-middle shadow-xl transition-all">
-                      <div class="w-full max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow-md sm:p-6 md:p-8 dark:bg-gray-800 dark:border-gray-700">
-                        <form class="space-y-6" action="#">
-                          <h5 class="text-xl font-medium text-gray-900 dark:text-white">
-                            Sign in to our platform
-                          </h5>
-                          <div>
-                            <label
-                              for="email"
-                              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                              Your email
-                            </label>
-                            <input
-                              type="email"
-                              name="email"
-                              id="email"
-                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                              placeholder="name@company.com"
-                              required
-                            />
-                          </div>
-                          <div>
-                            <label
-                              for="password"
-                              class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-                            >
-                              Your password
-                            </label>
-                            <input
-                              type="password"
-                              name="password"
-                              id="password"
-                              placeholder="••••••••"
-                              class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
-                              required
-                            />
-                          </div>
-                          <div class="flex items-start">
-                            <div class="flex items-start">
-                              <div class="flex items-center h-5">
-                                <input
-                                  id="remember"
-                                  type="checkbox"
-                                  value=""
-                                  class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
-                                  required
-                                />
-                              </div>
-                              <label
-                                for="remember"
-                                class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-                              >
-                                Remember me
-                              </label>
-                            </div>
-                            <a
-                              href="#"
-                              class="ml-auto text-sm text-blue-700 hover:underline dark:text-blue-500"
-                            >
-                              Lost Password?
-                            </a>
-                          </div>
+                      <form class="space-y-6" action="#">
+                        <h5 class="text-xl font-medium text-gray-900 dark:text-white">
+                          Order Details 
+                        </h5>
+                        <div>
+                          <label
+                            for="email"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            User Name
+                          </label>
+                          <input
+                            type="email"
+                            name="email"
+                            id="email"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            placeholder="name@company.com"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Email address
+                          </label>
+                          <input
+                            type="password"
+                            name="password"
+                            id="password"
+                            placeholder="tesla@gmail.com"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Product
+                          </label>
+                          <input
+                            type="text"
+                            name="password"
+                            id="password"
+                            placeholder="tesla car"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Price
+                          </label>
+                          <input
+                            type="number"
+                            name="password"
+                            id="password"
+                            placeholder="223"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Phone Number
+                          </label>
+                          <input
+                            type="number"
+                            name="password"
+                            id="password"
+                            placeholder="223"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            required
+                          />
+                        </div>
+                        <div>
+                          <label
+                            for="password"
+                            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Meeting location
+                          </label>
+                          <input
+                            type="text"
+                            name="password"
+                            id="password"
+                            placeholder="223"
+                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                            required
+                          />
+                        </div>
+
+                        <div className="button-group flex gap-2">
                           <button
                             type="submit"
                             class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
                           >
-                            Login to your account
+                            Book the product
                           </button>
-                          <div class="text-sm font-medium text-gray-500 dark:text-gray-300">
-                            Not registered?{" "}
-                            <a
-                              href="#"
-                              class="text-blue-700 hover:underline dark:text-blue-500"
-                            >
-                              Create account
-                            </a>
-                          </div>
-                        </form>
-                      </div>
-
-                      <div className="mt-4">
-                        <button
-                          type="button"
-                          className="inline-flex justify-center rounded-md border border-transparent bg-blue-100 px-4 py-2 text-sm font-medium text-blue-900 hover:bg-blue-200 focus:outline-none focus-visible:ring-2 focus-visible:ring-blue-500 focus-visible:ring-offset-2"
-                          onClick={closeModal}
-                        >
-                          Got it, thanks!
-                        </button>
-                      </div>
+                          <button
+                            onClick={closeModal}
+                            type="submit"
+                            class="w-full text-white bg-red-600 hover:bg-red-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                          >
+                            Cancel
+                          </button>
+                        </div>
+                      </form>
                     </Dialog.Panel>
                   </Transition.Child>
                 </div>
